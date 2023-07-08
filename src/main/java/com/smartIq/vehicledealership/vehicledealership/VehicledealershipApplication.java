c4b91c02eee06a764a895d23128684d75ea4be10
Harun170518513 <yusufeksioglu53@gmail.com>
2023-07-08 13:24:52 +0300

    project initiliazing

---
+++ src/main/java/com/smartIq/vehicledealership/vehicledealership/VehicledealershipApplication.java
@@ -0,0 +1,13 @@
+package com.smartIq.vehicledealership.vehicledealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicledealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicledealershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartIq/vehicledealership/vehicledealership/VehicledealershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartIq.vehicledealership.vehicledealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicledealershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
